1d96dd7b0ea419685303540dfb16f64933a85578
zibs <elizibin@gmail.com>
2017-09-18 18:10:44 -0700

    update screens and app json

--- screens/RandomWordScreen.js
+++ screens/RandomWordScreen.js
@@ -1,40 +1,49 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import QuizActions from '../redux/QuizRedux'
-import * as Progress from 'react-native-progress'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { connect } from "react-redux";
+import QuizActions from "../redux/QuizRedux";
+import * as Progress from "react-native-progress";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
-import Touchable from 'react-native-platform-touchable'
+import Touchable from "react-native-platform-touchable";
 
 class RandomWordScreen extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       animating: false
-    }
+    };
   }
   static navigationOptions = {
     header: null
-  }
+  };
 
   componentWillReceiveProps(nextProps) {
     if (this.props.quiz.fetching && !nextProps.quiz.fetching) {
       setTimeout(() => {
-        this.setState({ animating: false })
-      }, 1500)
+        this.setState({ animating: false });
+      }, 1500);
     } else if (!this.props.quiz.fetching && nextProps.quiz.fetching) {
-      this.setState({ animating: true })
+      this.setState({ animating: true });
     }
   }
 
   render() {
-    const { randomWord, isRevealed, currentColour, quiz } = this.props
+    const { randomWord, isRevealed, currentColour, quiz } = this.props;
     if (quiz.fetching || this.state.animating) {
       return (
-        <View style={[styles.container, { justifyContent: 'center', alignItems: 'center' }]}>
-          <Progress.CircleSnail size={200} thickness={7} color={currentColour} />
+        <View
+          style={[
+            styles.container,
+            { justifyContent: "center", alignItems: "center" }
+          ]}
+        >
+          <Progress.CircleSnail
+            size={200}
+            thickness={7}
+            color={currentColour}
+          />
         </View>
-      )
+      );
     } else {
       return (
         <Touchable
@@ -42,46 +51,62 @@ class RandomWordScreen extends React.Component {
           onPress={this.handlePress}
           background={Touchable.Ripple(currentColour)}
           activeOpacity={0.5}
-          underlayColor="red">
+          underlayColor="red"
+        >
           <ScrollView>
             <View
               style={[
                 styles.center,
-                { marginTop: 44, marginBottom: 64, borderBottomWidth: 3, borderBottomColor: currentColour }
-              ]}>
-              <Text style={{ fontWeight: '500', fontSize: 34, flexWrap: 'wrap', flex: 1, marginLeft: 15 }}>
+                {
+                  marginTop: 44,
+                  marginBottom: 64,
+                  borderBottomWidth: 10,
+                  borderBottomColor: currentColour
+                }
+              ]}
+            >
+              <Text
+                style={{
+                  fontWeight: "500",
+                  fontSize: 54,
+                  flexWrap: "wrap",
+                  flex: 1,
+                  marginLeft: 15
+                }}
+              >
                 {randomWord.word}
               </Text>
             </View>
-            {isRevealed &&
+            {isRevealed && (
               <View style={[styles.center, { marginHorizontal: 15 }]}>
-                <Text style={{ fontSize: 22, flexWrap: 'wrap', flex: 1 }}>
+                <Text style={{ fontSize: 24, flexWrap: "wrap", flex: 1 }}>
                   {randomWord.definition}
                 </Text>
-              </View>}
+              </View>
+            )}
           </ScrollView>
         </Touchable>
-      )
+      );
     }
   }
 
   handlePress = () => {
-    const { isRevealed, dispatch } = this.props
+    const { isRevealed, dispatch } = this.props;
     if (isRevealed) {
-      dispatch(QuizActions.resetRandomWord())
+      dispatch(QuizActions.resetRandomWord());
     } else {
-      dispatch(QuizActions.revealDefinition())
+      dispatch(QuizActions.revealDefinition());
     }
-  }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 24,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   }
-})
+});
 
 const mapStateToProps = state => {
   return {
@@ -91,7 +116,7 @@ const mapStateToProps = state => {
     quiz: state.quiz,
     isRevealed: state.quiz.revealed,
     currentColour: state.static.currentColour
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(RandomWordScreen)
+export default connect(mapStateToProps)(RandomWordScreen);
--- screens/WordScreen.js
+++ screens/WordScreen.js
@@ -1,52 +1,74 @@
-import React from 'react'
-import { Image, Platform, StyleSheet, Text, View, Button, ScrollView } from 'react-native'
-import { connect } from 'react-redux'
-import StartupActions from '../redux/StartupRedux'
-import WordActions from '../redux/WordsRedux'
-import Touchable from 'react-native-platform-touchable'
+import React from "react";
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView
+} from "react-native";
+import { connect } from "react-redux";
+import StartupActions from "../redux/StartupRedux";
+import WordActions from "../redux/WordsRedux";
+import Touchable from "react-native-platform-touchable";
 import { BlurView } from "expo";
-import R from 'ramda'
+import R from "ramda";
 
 class WordsScreen extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   static navigationOptions = {
     header: null
-  }
+  };
 
   render() {
-    const { currentWord, currentColour } = this.props
-    const { navigation: { state: { params } } } = this.props
+    const { currentWord, currentColour } = this.props;
+    const { navigation: { state: { params } } } = this.props;
     return (
       <ScrollView style={styles.container} contentContainerStyle={{}}>
         <View
           style={[
             styles.center,
-            { marginTop: 44, marginBottom: 64, borderBottomWidth: 10, borderBottomColor: currentColour }
-          ]}>
-          <Text style={{ fontWeight: '500', fontSize: 54, flexWrap: 'wrap', flex: 1, marginHorizontal: 15 }}>
+            {
+              marginTop: 44,
+              marginBottom: 64,
+              borderBottomWidth: 10,
+              borderBottomColor: currentColour
+            }
+          ]}
+        >
+          <Text
+            style={{
+              fontWeight: "500",
+              fontSize: 54,
+              flexWrap: "wrap",
+              flex: 1,
+              marginHorizontal: 15
+            }}
+          >
             {currentWord.word || params.id}
           </Text>
         </View>
-        <View style={[styles.center, {marginHorizontal: 15}]}>
-          <Text style={{ fontSize: 24, flexWrap: 'wrap', flex: 1 }}>
+        <View style={[styles.center, { marginHorizontal: 15 }]}>
+          <Text style={{ fontSize: 24, flexWrap: "wrap", flex: 1 }}>
             {currentWord.definition || params.definition}
           </Text>
         </View>
       </ScrollView>
-    )
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 24,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   }
-})
+});
 
 const mapStateToProps = state => {
   return {
@@ -54,6 +76,6 @@ const mapStateToProps = state => {
     currentWord: state.words.word,
     colours: state.static.colours,
     currentColour: state.static.currentColour
-  }
-}
-export default connect(mapStateToProps)(WordsScreen)
+  };
+};
+export default connect(mapStateToProps)(WordsScreen);
